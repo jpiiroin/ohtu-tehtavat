67cdb6f7cd9b3c14bc85e5a777cd89a835222c5d
Janne Piiroinen <jpiiroin@hotmail.com>
2021-02-22 17:33:28 +0200

    riippuvuudet t12

--- viikko2/verkkokauppa-1/src/pankki.py
+++ viikko2/verkkokauppa-1/src/pankki.py
@@ -1,9 +1,8 @@
-from kirjanpito import Kirjanpito
-
+from kirjanpito import kirjanpito as default_kirjanpito
 
 class Pankki:
-    def __init__(self, Kirjanpito):
-        self._kirjanpito = Kirjanpito
+    def __init__(self, kirjanpito=default_kirjanpito):
+        self._kirjanpito = kirjanpito
 
     def tilisiirto(self, nimi, viitenumero, tililta, tilille, summa):
         self._kirjanpito.lisaa_tapahtuma(
@@ -12,3 +11,5 @@ class Pankki:
 
         # täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
         return True
+
+pankki = Pankki()
